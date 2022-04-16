122729cdfe82dfdc43f38c80b8a416498d0591c0
mikitaro-27 <matt.diak@gmail.com>
2022-04-16 14:59:15 +0100

    Creates a class component for Details

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,48 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 
-const Details = () => {
-  const { id } = useParams();
+class Details extends Component {
+  constructor(props) {
+    super(props);
 
-  console.log(id);
+    this.state = { loading: true };
+  }
 
-  return <div>Hello</div>;
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    // this.setState(Object.assign({ loading: false }, json.pets[0]));
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
