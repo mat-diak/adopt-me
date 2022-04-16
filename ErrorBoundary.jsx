fbe18bc5e67b0677c5eb2c26080146623590cc07
mikitaro-27 <matt.diak@gmail.com>
2022-04-16 19:29:34 +0100

    Implement ErrorBoundary component

---
+++ ErrorBoundary.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          There was an error.
+          <Link to="/">Click here</Link> to go to homepage or wait 5s
+        </h2>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
