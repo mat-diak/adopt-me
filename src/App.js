1abab9eccaeb078a4eba1b7f3b4c8b680c3c7be3
mikitaro-27 <matt.diak@gmail.com>
2022-04-16 14:35:36 +0100

    Creates detailed page for each pet

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { render } from "react-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details.jsx";
 
 const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
-        <h1>Adopt me!</h1>
+        <header>
+          <Link to="/">Adopt me!</Link>
+        </header>
+
         <Routes>
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,4 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const Details = () => {
+  const { id } = useParams();
+
+  console.log(id);
+
   return <div>Hello</div>;
 };
 
