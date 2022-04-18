0429c6d843880655852f729f39a89e3e355ea230
mikitaro-27 <matt.diak@gmail.com>
2022-04-18 11:42:13 +0100

    Sets up ssr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import { render } from "react-dom";
 import { StrictMode, useState, lazy, Suspense } from "react";
 import ThemeContext from "./ThemeContext.jsx";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 const Details = lazy(() => import("./Details.jsx"));
 const SearchParams = lazy(() => import("./SearchParams.jsx"));
@@ -12,19 +11,17 @@ const App = () => {
     <StrictMode>
       <Suspense fallback={<h2>loading, be patient</h2>}>
         <ThemeContext.Provider value={theme}>
-          <BrowserRouter>
-            <header>
-              <Link to="/">Adopt me!</Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
-          </BrowserRouter>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
         </ThemeContext.Provider>
       </Suspense>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
